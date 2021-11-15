7a51f746acb1fdf697b17956c22af20e2218106e
Lucas Pereira <lucasgpereira.10@gmail.com>
2021-11-15 17:06:52 +0100

    Setting up the project

---
+++ Ripple/Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by Lucas Pereira on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
