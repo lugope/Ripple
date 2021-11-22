d4ee7a831f8fe337a0bf0c16fbb9f71313431afb
Antonio Giulio Mele <antoniogiuliomele@10-20-51-205.0200.fed.it.iosda.org>
2021-11-22 18:11:41 +0100

    Adding sounds view and sound model

---
+++ Ripple/Ripple/Model/Sound.swift
@@ -0,0 +1,20 @@
+//
+//  Sound.swift
+//  Ripple
+//
+//  Created by Antonio Giulio Mele on 22/11/21.
+//
+
+import SwiftUI
+
+struct Sound: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sound_Previews: PreviewProvider {
+    static var previews: some View {
+        Sound()
+    }
+}
---
+++ Ripple/Ripple/View/SoundList.swift
@@ -0,0 +1,20 @@
+//
+//  SoundList.swift
+//  Ripple
+//
+//  Created by Antonio Giulio Mele on 22/11/21.
+//
+
+import SwiftUI
+
+struct SoundList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SoundList_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundList()
+    }
+}
