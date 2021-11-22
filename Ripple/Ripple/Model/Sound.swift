840ebd0ab8115bc1c888e653ed3d1f8405427f98
Antonio Giulio Mele <antoniogiuliomele@10-20-51-205.0200.fed.it.iosda.org>
2021-11-22 18:13:45 +0100

    Adding sound view and model

--- Ripple/Ripple/Model/Sound.swift
+++ Ripple/Ripple/Model/Sound.swift
@@ -5,16 +5,10 @@
 //  Created by Antonio Giulio Mele on 22/11/21.
 //
 
-import SwiftUI
+import Foundation
 
-struct Sound: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct sound: Identifiable {
+    var id = UUID()
+    var name:String
 }
 
-struct Sound_Previews: PreviewProvider {
-    static var previews: some View {
-        Sound()
-    }
-}
--- Ripple/Ripple/View/SoundList.swift
+++ Ripple/Ripple/View/SoundList.swift
@@ -5,16 +5,82 @@
 //  Created by Antonio Giulio Mele on 22/11/21.
 //
 
+import Foundation
 import SwiftUI
 
-struct SoundList: View {
+
+struct SoundTest: Identifiable {
+    var id = UUID()
+    var name:String
+}
+
+let soundName:[SoundTest] = [
+    SoundTest(name:"Waves"),
+    SoundTest(name:"Wind"),
+    SoundTest(name:"Bruh Sound Effect #2"),
+    SoundTest(name:"Vuoi Farla Ingelosirla?")
+]
+
+private struct RowContent: View {
+    var sound: SoundTest
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {}) {
+                HStack{
+                Text(sound.name)
+                Spacer()
+                Image(systemName: "checkmark")
+            }
+        }
+            .listRowBackground(Color(red: 251/255, green: 251/255, blue: 253/255, opacity: 100))
+            .foregroundColor(Color(red: 35/255, green: 86/255, blue: 150/255, opacity: 100))
+        
     }
+    
+}
+
+struct SoundListView: View {
+    
+    init() {
+        
+        coloredNavAppearance.configureWithOpaqueBackground()
+        coloredNavAppearance.backgroundColor = UIColor(Color(red: 236/255, green: 238/255, blue: 244/255, opacity: 100))
+        coloredNavAppearance.titleTextAttributes = [.foregroundColor: UIColor(Color(red: 94/255, green: 168/255, blue: 251/255, opacity: 100))]
+        coloredNavAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().standardAppearance = coloredNavAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
+        
+        UITableView.appearance().backgroundColor = UIColor(Color(red: 236/255, green: 238/255, blue: 244/255, opacity: 100))
+        
+    }
+    
+    var body: some View {
+                
+        
+        List {
+            Section(
+                header:
+
+                        Text("Your Sounds")
+                            .foregroundColor(Color(red: 94/255, green: 168/255, blue: 251/255, opacity: 100))
+                    
+            ) {
+                ForEach( soundName, id:\.id) { e in
+                    RowContent(sound: e)
+                }
+            } // End Section
+        } // End List
+            .navigationTitle("Sounds")
+            .navigationBarTitleDisplayMode(.inline)
+        
+    }
+    
 }
 
 struct SoundList_Previews: PreviewProvider {
     static var previews: some View {
-        SoundList()
+        SoundListView()
+            .previewDevice("iPhone 11")
     }
 }
